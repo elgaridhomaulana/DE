ebc7f9cc369823cee24b46c9848519f59db3705a
elgaridhomaulana <elgaridhomaulana@gmail.com>
2020-12-24 00:05:40 +0700

    update readme and add setup and teardown

--- unit_testing/note/unit_test.md
+++ unit_testing/note/unit_test.md
@@ -9,8 +9,27 @@ To find bugs in functions, you need to follow a four step procedure.
 <ol>Fix the bugs.</ol>
 </li>
 
-In a previous exercise, you wrote a unit test for the function convert_to_int(), which is supposed to convert a comma separated integer string like "2,081" to the integer 2081. You also ran the unit test and discovered that it is failing.
+## Running Tests
 
-In this exercise, you will read the test result report from that exercise in detail, and then spot and fix the bug. This would equip you with all basic skills to start using unit tests for your projects.
+### Running all tests
+cd tests
+pytest
 
-The convert_to_int() function is defined in the file preprocessing_helpers.py. The unit test is available in the test module test_convert_to_int.py.
+### Running all tests but stop after found an error
+pytest -x
+execution of test stop when encounter one error
+
+### Running test on a specific file contains test
+pytest data/test_preprocessing_helpers.py
+
+### Running test on specific test class
+pytest data/test_processing_helpers.py::TestRowToList
+will running 7 tests because there is 7 test in class TestRowToList
+
+pytest data/test_processing_helpers.py::TestRowToList::test_on_one_tab_with_missing_value
+will running only 1 test
+
+### Running test with keyword extraction
+pytest -k "TestSplitIntoTrainingAndTestingSets"
+or
+pytest -k "TestSplit"
--- unit_testing/tests/features/test_as_numpy.py
+++ unit_testing/tests/features/test_as_numpy.py
@@ -1,11 +1,34 @@
 from features.as_numpy import get_data_as_numpy_array
 import numpy as np
 import pytest
+import os
 
 
+# pytest fixture dapat digunakan untuk proses setup dan teardown
+@pytest.fixture
+def clean_data_file():
+    # setup file_path
+    file_path = "clean_data_file.txt"
+    # menuliskan nilai ke dalam file txt dengan context manager
+    with open(file_path, "w") as f:
+        f.write("2081.0\t314942.0\n1059.0\t186606.0\n1148.0\t206186.0\n")
+    yield file_path
+    # menghapus apabila sudah selesai digunakan
+    os.remove(file_path)
+
+
+@pytest.fixture
+def empty_file(tmpdir):
+    file_path = tmpdir.join("empty.txt")
+    open(file_path, "w").close()
+    yield file_path
+
+
+# pytest mark.usefixtures digunakan untuk menggunakan fixture dalam class
+@pytest.mark.usefixtures("clean_data_file", "empty_file")
 class TestGetDataAsNumpyArray(object):
 
-    def test_on_clean_file(self):
+    def test_on_clean_file(self, clean_data_file):
         expected = np.array(
             [
                 [2081.0, 314942.0],
@@ -14,6 +37,12 @@ class TestGetDataAsNumpyArray(object):
             ]
         )
 
-        actual = get_data_as_numpy_array("../data/example_clean_data_file.txt", num_columns=2)
+        actual = get_data_as_numpy_array(clean_data_file, num_columns=2)
+        message = "Expected return value: {0}, Actual return value: {1}".format(expected, actual)
+        assert actual == pytest.approx(expected), message
+
+    def test_on_empty_file(self, empty_file):
+        expected = np.empty((0, 2))
+        actual = get_data_as_numpy_array(empty_file, 2)
         message = "Expected return value: {0}, Actual return value: {1}".format(expected, actual)
         assert actual == pytest.approx(expected), message
