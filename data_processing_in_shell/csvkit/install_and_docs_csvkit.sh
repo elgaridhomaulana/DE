25d14378eb7cdc3345bde89ceb49b1bc7a162bde
elgaridhomaulana <elgaridhomaulana@gmail.com>
2020-12-26 19:09:22 +0700

    menambahkan file csvkit dengan command line

---
+++ data_processing_in_shell/csvkit/install_and_docs_csvkit.sh
@@ -0,0 +1,20 @@
+# Upgrade csvkit using pip  
+pip install --upgrade csvkit
+
+# Print manual for in2csv
+in2csv -h
+
+# Print manual for csvlook
+csvlook -h
+
+# Print manual for csvstat
+csvstat -h
+
+# Print manual for csvcut
+csvcut -h
+
+# Print manual for csvgrep
+csvgrep -h
+
+# Print manual for csvstack
+csvstack -h
